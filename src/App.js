40bc7ddf363e159d0fa668ea8d764e83ee214a85
sofia-pastor <sofiapastor93@gmail.com>
2024-11-30 23:14:40 +0000

    added bootstrap and axios

--- src/App.js
+++ src/App.js
@@ -3,28 +3,30 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <p>
-          This project was coded by{" "}
-          <a
-            href="https://github.com/sofia-pastor/weather-react-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Sofia Pastor
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/sofia-pastor/weather-react-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-          .
-        </p>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/sofia-pastor/weather-react-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Sofia Pastor
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/sofia-pastor/weather-react-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+            .
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
